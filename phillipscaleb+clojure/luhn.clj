0e18d1b3905f8bdcaae3cf5483399119b9206dd3
calebphillips <caleb.phillips@gmail.com>
2012-03-25 22:34:09 -0500

    handle overlapping numbers

--- phillipscaleb+clojure/luhn.clj
+++ phillipscaleb+clojure/luhn.clj
@@ -38,42 +38,52 @@
    of n characters, not counting spaces and hyphens, but preserving them so that
    we can spit them back out once we mask any cc numbers
    A lot of this is copied from the partition function, so there maybe a better
-   way to reuse something existing."
-  [n step ignore coll]
-  (let [cnt-ignoring #(count (remove ignore %))
-        full-partition? #(= n (cnt-ignoring %))
-        take-ignoring #(loop [c % taken []]
-                         (if (or (full-partition? taken) (empty? c))
-                           taken
-                           (recur (rest c) (conj taken (first c)))))]
-    (lazy-seq
-     (when-let [s (seq coll)]
-       (let [p (take-ignoring s)]
-         (when (full-partition? p)
-           (cons p (partition-ignoring n step ignore (nthnext s step)))))))))
+   way to reuse something existing.
+
+   Is there a cleaner way to do this with regex?
+  "
+  ([n step ignore coll]
+     (partition-ignoring n step ignore coll 0))
+  ([n step ignore coll pos]
+     (let [cnt-ignoring #(count (remove ignore %))
+           full-partition? #(= n (cnt-ignoring %))
+           take-ignoring #(loop [c % taken []]
+                            (if (or (full-partition? taken) (empty? c))
+                              taken
+                              (recur (rest c) (conj taken (first c)))))]
+       (lazy-seq
+        (when-let [s (seq coll)]
+          (let [p (take-ignoring s)]
+            (when (full-partition? p)
+              (cons [p pos] (partition-ignoring
+                             n step ignore (nthnext s step) (inc pos))))))))))
 
 (defn find-cc-nums
   "Return a lazy seq of the credit card numbers in s.  The numbers will include
    all the allowed characters for a credit card number according to the spec:
    digits, spaces and hyphens."
   [s]
-  (map #(apply str %)
-       (filter cc?
-               (partition-ignoring 16 1 #{\- \space} s))))
+  (map (fn [[num pos]] [(apply str num) pos])
+       (filter #(cc? (first %))
+               (mapcat #(partition-ignoring % 1 #{\- \space} s)
+                       [14 15 16]))))
 
 (defn mask-cc-num
   "Return the string s with cc replaced with its masked form"
   [s cc]
-  (let [masked-cc (cs/replace cc #"\d" "X")]
-    (cs/replace s cc masked-cc)))
+  (let [[num pos] cc
+        masked-cc (cs/replace num #"\d" "X")
+        before (subs s 0 pos)
+        after (subs s (+ pos (count masked-cc)))]
+    (str before masked-cc after)))
 
 (defn mask
   "Takes a string s and returns a string with the credit card numbers masked.
 
-  This is a very simple version so far that does not cover many of the cases
-  listed in the problem spec."
-  [s]
-  (loop [s s ccs (find-cc-nums s)]
-    (if (empty? ccs)
-      s
-      (recur (mask-cc-num s (first ccs)) (rest ccs)))))
+   Takes a pretty brute force approach.  Also does not read streams as the
+   square program expects.
+  "
+  [text]
+  (reduce (fn [s cc] (mask-cc-num s cc))
+          text
+          (find-cc-nums s)))
