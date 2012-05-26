a705cdc31462667393b0ee7e513706f022fbaff4
Calvin Bottoms <calvin.bottoms@gmail.com>
2012-05-26 08:25:22 -0500

    Passes tests now. Mean time 315 ms.

--- calvinb+haskell/luhn.hs
+++ calvinb+haskell/luhn.hs
@@ -1,20 +1,51 @@
--- Didn't bother with the whole file thing yet.
--- Here are just the functions for the Luhn check and the credit card format.
-
 import Data.Char
+import System.IO
 
-isLuhn :: [Char] -> Bool
-isLuhn digits = 
-  let 
-    digitSum x  | x < 10 = x
-                | otherwise = x `mod` 10 + digitSum (x `div` 10)
-    withDoubles = reverse $ zipWith ($) (cycle [id, (*2)]) (reverse $ map digitToInt digits)
-    digitSumTotal = sum $ map digitSum withDoubles
-  in digitSumTotal `mod` 10 == 0
+minLen = 14
+maxLen = 16
 
-isCreditCard :: [Char] -> Bool
-isCreditCard s =
-  let
-    legalChar x = isDigit x || x `elem` "- "
-    digits = filter isDigit s
-  in all legalChar s && length digits `elem` [14..16] && isLuhn digits
+finalChar :: (Char, Bool) -> Char
+finalChar (c, x) = if x then 'X' else c
+
+doubleDigitSum :: Int -> Int
+doubleDigitSum i =
+    let twice = i * 2
+    in twice `div` 10 + twice `mod` 10
+
+luhn :: String -> String
+luhn cs =
+    let luhn' :: [(Char, Bool)] -> [(Char, Bool)] -> Int -> Int -> String -> String
+        luhn' [] bs _ _ result = (map finalChar bs) ++ result
+        luhn' cs bs digitCount _ result
+            | digitCount == maxLen =
+                let (b:bs') = reverse bs
+                    result' = ((finalChar b):result)
+                    cs' = bs' ++ cs
+                in  luhn' cs' [] 0 0 result'
+        luhn' ((c, x):cs) bs digitCount digitSum result
+            | isDigit c =
+                let digitCount' = digitCount + 1
+                    digitSummer = if digitCount' `mod` 2 == 0
+                                    then doubleDigitSum
+                                    else id
+                    digitSum' = digitSum + digitSummer (read [c])
+                in  if digitCount' < minLen
+                    then luhn' cs ((c, x):bs) digitCount' digitSum' result
+                    else
+                        let xer = if digitSum' `mod` 10 == 0
+                                    then map (\(c, x) -> (c, isDigit c))
+                                    else id
+                            bs' = xer ((c, x):bs)
+                        in luhn' cs bs' digitCount' digitSum' result
+            | otherwise = luhn' cs ((c, x):bs) digitCount digitSum result
+
+    in luhn' (reverse $ map (\c -> (c, False)) cs) [] 0 0 ""
+
+main = do
+    input <- getLine
+    if null input
+    then return ()
+    else do
+        putStrLn $ luhn input
+        hFlush stdout
+        main
