e6cf239a2fc143c5426580bd3cdc1af6bad88c00
Calvin Bottoms <calvin.bottoms@fireflylogic.com>
2013-06-04 06:11:27 -0500

    Check each character for digit only once.

--- calvinb+erlang/luhn.erl
+++ calvinb+erlang/luhn.erl
@@ -13,66 +13,53 @@ process_line(Line) ->
     io:fwrite("~s", [transform_line(Line)]),
     start().
 
-is_digit($0) -> true;
-is_digit($1) -> true;
-is_digit($2) -> true;
-is_digit($3) -> true;
-is_digit($4) -> true;
-is_digit($5) -> true;
-is_digit($6) -> true;
-is_digit($7) -> true;
-is_digit($8) -> true;
-is_digit($9) -> true;
-is_digit(_) -> false.
+transform_line(Line) -> transform_line(eval_digits(Line), 0, "").
 
-% To start, call split_on_digit and toss a chunk straight to the result.
-% Then, from the first digit, call max_luhn_length on the line.
-% Use the greater of this call and the accumulator max.
-% If it's zero, send the first character through to the result, and recurse the rest.
-% If it's non-zero, send an X to the result and recurse the rest with max - 1.
+eval_digits(List) ->
+    lists:map(fun maybe_eval_digit/1, List).
 
-transform_line(Line) -> transform_line(Line, 0, "").
+maybe_eval_digit(Char) when Char > $9 orelse Char < $0 ->
+    Char;
+maybe_eval_digit(Char) ->
+    {Char, Char - $0}.
 
-transform_line("", _, Result) -> lists:reverse(Result);
+transform_line([], _, Result) -> lists:reverse(Result);
+transform_line([{Digit, Value}|Tail], MaskCount, Result) -> 
+    {NewMaskCount, ResultChar} = get_new_mask_and_result_char([{Digit, Value}|Tail], MaskCount),
+    transform_line(Tail, NewMaskCount, [ResultChar|Result]);
 transform_line([Head|Tail], MaskCount, Result) -> 
-    transform_line([Head|Tail], MaskCount, Result, is_digit(Head)).
-
-transform_line(List, MaskCount, Result, true) ->
-    transform_line_from_digit(List, MaskCount, Result);
-transform_line([Head|Tail], MaskCount, Result, false) ->
     transform_line(Tail, MaskCount, [Head|Result]).
 
-transform_line_from_digit([Digit|Rest], MaskCount, Result) ->
-    {NewMaskCount, ResultChar} = get_new_mask_and_result_char([Digit|Rest], MaskCount),
-    transform_line(Rest, NewMaskCount, [ResultChar|Result]).
-
-get_new_mask_and_result_char([Char|Rest], MaskCount) ->
-    max_if_nonzero(max(max_luhn_length([Char|Rest]), MaskCount), Char).
+get_new_mask_and_result_char([{Digit, Value}|Tail], MaskCount) ->
+    max_if_nonzero(max(max_luhn_length([{Digit, Value}|Tail]), MaskCount), Digit).
 
 max_if_nonzero(0, Char) -> {0, Char};
 max_if_nonzero(ThisMaskCount, _) -> {ThisMaskCount - 1, $X}.
 
-max_luhn_length(String) ->
-    first_or_zero(lists:filter(fun(X) -> is_luhn(String, X) end, reverse_range())).
+max_luhn_length(List) ->
+    first_or_zero(lists:filter(fun(X) -> is_luhn(List, X) end, reverse_range())).
 
 first_or_zero([]) -> 0;
 first_or_zero([Max|_]) -> Max.
 
 reverse_range() -> lists:reverse(lists:seq(?MinLength, ?MaxLength)).
 
-is_luhn(String, DigitCount) -> is_luhn(find_digits(String, DigitCount)).
+is_luhn(List, DigitCount) -> is_luhn(find_digits(List, DigitCount)).
 
-is_luhn(String) -> ok_and_divisible(string_digit_sum(String)).
+is_luhn(List) -> ok_and_divisible(string_digit_sum(List)).
 
 ok_and_divisible({ok, Sum}) -> Sum rem 10 =:= 0;
 ok_and_divisible(_) -> false.
 
-string_digit_sum(String) -> 
-    sum_digit_list(lists:filter(fun is_digit/1, String)).
+string_digit_sum(List) -> 
+    sum_digit_list(lists:filter(fun is_digit/1, List)).
 
-sum_digit_list("") -> no_digits;
-sum_digit_list(Digits) ->
-    Values = lists:map(fun(D) -> D - $0 end, Digits),
+is_digit({_, _}) -> true;
+is_digit(_) -> false.
+
+sum_digit_list([]) -> no_digits;
+sum_digit_list(List) ->
+    Values = lists:map(fun({_, Value}) -> Value end, List),
     {Sum, _} = lists:foldr(fun digit_sum_fold/2, {0, false}, Values),
     {ok, Sum}.
 
@@ -82,12 +69,11 @@ digit_sum_fold(Elem, {Sum, Double}) ->
 digit_sum(X, true) -> (X * 2 - 1) rem 9 + 1;
 digit_sum(X, false) -> X.
 
-find_digits(Line, Count) -> find_digits(Line, Count, "").
+find_digits(List, Count) -> find_digits(List, Count, []).
 
 find_digits(_, 0, Result) -> lists:reverse(Result);
-find_digits("", _, _) -> "";
+find_digits([], _, _) -> "";
+find_digits([{Digit, Value}|Tail], Count, Result) -> 
+    find_digits(Tail, Count - 1, [{Digit, Value}|Result]);
 find_digits([Head|Tail], Count, Result) -> 
-    find_digits(Tail, decrement_maybe(Count, is_digit(Head)), [Head|Result]).
-
-decrement_maybe(Count, true) -> Count - 1;
-decrement_maybe(Count, false) -> Count.
+    find_digits(Tail, Count, [Head|Result]).
